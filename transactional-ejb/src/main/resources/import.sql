9215e644efd1c9eaf18cd26993a9a11d168404e2
Roman Tokarev <r.tokarev@touchbank.com>
2017-07-31 11:43:17 +0300

    Hotfix #1.

--- transactional-ejb/src/main/resources/import.sql
+++ transactional-ejb/src/main/resources/import.sql
@@ -15,9 +15,6 @@
 -- limitations under the License.
 --
 
--- You can use this file to load seed data into the database using SQL statements
-insert into Registrant(id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
-
 insert into Client(id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212')
 
 insert into Client(id, name, email, phone_number) values (1, 'James Bond', 'james.bond@mailinator.com', '2125551212')
